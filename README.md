b1ad63009fc2678b1cb70a1697b35eb590658f25
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-01-10 22:50:36 -0600

    Create index.html and getComputerChoice function

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-rock-paper-scissors
+
+My take on the rock-paper-scissor project as a part of the Odin Project curriculum.
---
+++ script.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    let rand = Math.random();
+    if (rand < 1/3) {
+        return "rock";
+    }
+    else if (rand < 2/3) {
+        return "paper";
+    }
+    return "scissor";
+}
