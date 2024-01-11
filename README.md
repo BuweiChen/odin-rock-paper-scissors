982c39fdc6f2a9156757f68d92b5cdd82ed5fb5f
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-01-10 22:57:40 -0600

    Add playRound function

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # odin-rock-paper-scissors
 
-My take on the rock-paper-scissor project as a part of the Odin Project curriculum.
+My take on the rock-paper-scissors project as a part of the Odin Project curriculum.
--- script.js
+++ script.js
@@ -6,5 +6,48 @@ function getComputerChoice() {
     else if (rand < 2/3) {
         return "paper";
     }
-    return "scissor";
+    return "scissors";
+}
+
+function playRound(playerSelection, computerSelection) {
+    switch (playerSelection.toLowerCase()) {
+        case "rock":
+            switch (computerSelection) {
+                case "rock":
+                    return "it's a tie.";
+                    
+                case "paper":
+                    return "you lose.";
+                
+                case "scissors":
+                    return "you win.";
+            }
+            break;
+        case "paper":
+            switch (computerSelection) {
+                case "rock":
+                    return "you win.";
+                    
+                case "paper":
+                    return "it's a tie.";
+                
+                case "scissors":
+                    return "you lose.";
+            }
+            break;
+        case "scissors":
+            switch (computerSelection) {
+                case "rock":
+                    return "you lose.";
+                    
+                case "paper":
+                    return "you win.";
+                
+                case "scissors":
+                    return "it's a tie.";
+            }
+            break;
+        default:
+            return "please type either rock, paper, or scissors."
+    }
 }
